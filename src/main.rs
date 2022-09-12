2e8c6419b1221ce3186c672b7890ca3bdace5524
dtc103 <jan_kerner@gmx.de>
2022-09-12 02:37:55 +0200

    added chaos game picture generation algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,18 @@
 use image::*;
 
 mod picture_manipulation;
+use picture_manipulation::image_functions::misc::open_picture;
 use picture_manipulation::image_functions::filter::filter3x3;
 use picture_manipulation::image_functions::filter::grey_filter;
-use picture_manipulation::image_functions::fractals::chaos_game;
+
+//use picture_manipulation::image_functions::fractals::chaos_game;
 
 use std::io;
 
 fn main() {
-    //let rgb_image = open_picture();
-    let image = chaos_game((512, 512), 100000000);
+    let rgb_image = open_picture();
 
-    image
+    rgb_image
         .save_with_format("fractal.png", ImageFormat::Png)
         .expect("Fractal was generated, but saving failed");
-}
-
-fn open_picture() -> RgbImage {
-    println!("Pfad des Bildes eingeben: ");
-    let mut path = String::new();
-    io::stdin()
-        .read_line(&mut path)
-        .expect("Path Reading went wrong");
-    path = String::from(path.trim());
-
-    let images = image::open(path).expect("something went wrong");
-
-    images.into_rgb()
 }
--- src/picture_manipulation.rs
+++ src/picture_manipulation.rs
@@ -1,7 +1,7 @@
 pub mod image_functions {
 
     pub mod misc {
-        fn open_picture() -> image::RgbImage {
+        pub fn open_picture() -> image::RgbImage {
             println!("Pfad des Bildes eingeben: ");
             let mut path = String::new();
             std::io::stdin()
@@ -13,7 +13,150 @@ pub mod image_functions {
 
             images.into_rgb()
         }
+
+        pub fn hsv_to_rgb(mut h: f32, mut s: f32, mut v: f32) -> (u8, u8, u8) {
+            while h >= 360f32 {
+                h -= 360f32;
+            }
+            if h < 0f32 {
+                h = 0f32;
+            }
+        
+            if s < 0f32 {
+                s = 0f32;
+            }
+            if s > 1f32 {
+                s = 1f32;
+            }
+        
+            if v < 0f32 {
+                v = 0f32;
+            }
+            if v > 1f32 {
+                v = 1f32;
+            }
+        
+            let c = v * s;
+            let x = c * (1f32 - ((h / 60f32) % 2f32 - 1f32).abs());
+            let m = v - c;
+        
+            //h has to be in the interval [0, 360) (because of the first while loop in this function)
+            // the conversion to u32 also leads to the fact, that we cant use all the values on the circle, but only 360
+            match h as u32 {
+                0..=59 => (
+                    ((c + m) * 255f32) as u8,
+                    ((x + m) * 255f32) as u8,
+                    (m * 255f32) as u8,
+                ),
+                60..=119 => (
+                    ((x + m) * 255f32) as u8,
+                    ((c + m) * 255f32) as u8,
+                    (m * 255f32) as u8,
+                ),
+                120..=179 => (
+                    (m * 255f32) as u8,
+                    ((c + m) * 255f32) as u8,
+                    ((x + m) * 255f32) as u8,
+                ),
+                180..=239 => (
+                    (m * 255f32) as u8,
+                    ((x + m) * 255f32) as u8,
+                    ((c + m) * 255f32) as u8,
+                ),
+                240..=299 => (
+                    ((x + m) * 255f32) as u8,
+                    (m * 255f32) as u8,
+                    ((c + m) * 255f32) as u8,
+                ),
+                300..=359 => (
+                    ((c + m) * 255f32) as u8,
+                    (m * 255f32) as u8,
+                    ((x + m) * 255f32) as u8,
+                ),
+                _ => panic!("This case shouldnt be happen"),
+            }
+        }
     }
+
+    pub mod picture_generation{
+        use rand;
+        use rand::prelude::*;
+        use rand::seq::SliceRandom;
+        use super::misc::hsv_to_rgb;
+
+        pub fn chaos_game(
+            picture_size: (u32, u32),
+            iteration_depth: u32,
+            input_points: Option<Vec<(u32, u32)>>,
+            colored: bool,
+            mut relative_jump: f32, //should be bigger than 1
+        ) -> Option<image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>> {
+            let mut image = image::RgbImage::new(picture_size.0, picture_size.1);
+        
+            if relative_jump <= 1f32 {
+                relative_jump = 1f32;
+            }
+        
+            let steps = iteration_depth;
+        
+            //only use these 3 points, if the user doesnt provide own points
+            let a: (u32, u32);
+            let b: (u32, u32);
+            let c: (u32, u32);
+            let mut point_array = Vec::new();
+            match input_points {
+                Some(mut input_list) => {
+                    for point in &input_list {
+                        //wont check for < 0, because the type checker will throw an exception if that happens.
+                        if point.0 > image.width() || point.1 > image.height() {
+                            return None;
+                        }
+                    }
+                    point_array.append(&mut input_list);
+                }
+                None => {
+                    a = (image.width() / 2, (image.height() as f32 * 0.02) as u32);
+                    b = (
+                        (image.width() as f32 * 0.02) as u32,
+                        (image.height() as f32 * 0.98) as u32,
+                    );
+                    c = (
+                        (image.width() as f32 * 0.98) as u32,
+                        (image.height() as f32 * 0.98) as u32,
+                    );
+                    point_array.append(&mut vec![a, b, c]);
+                }
+            };
+            //first point is in the middle of the picture(Could be at random position, but this doesnt rly matter).
+            let mut point = (image.width() / 2, image.height() / 2);
+        
+            let mut rnd = thread_rng();
+        
+            for _ in 0..steps {
+                let corner = point_array.as_slice().choose(&mut rnd).unwrap();
+                point = (
+                    (point.0 as i32 + ((corner.0 as i32 - point.0 as i32) as f32 / relative_jump) as i32)
+                        as u32,
+                    (point.1 as i32 + ((corner.1 as i32 - point.1 as i32) as f32 / relative_jump) as i32)
+                        as u32,
+                );
+        
+                if colored {
+                    let color: (u8, u8, u8) = hsv_to_rgb(
+                        360f32 * ((image.height() - point.1) as f32 / image.height() as f32),
+                        1f32,
+                        1f32,
+                    );
+                    image.put_pixel(point.0, point.1, image::Rgb([color.0, color.1, color.2]));
+                } else {
+                    image.put_pixel(point.0, point.1, image::Rgb([255, 255, 255]));
+                }
+            }
+        
+            Some(image)
+        }
+    }
+
     pub mod filter {
         pub fn grey_filter(
             img: image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>,
@@ -94,48 +237,6 @@ pub mod image_functions {
             img: image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>>,
         ) -> image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>> {
             image::ImageBuffer::new(1, 1)
-        }
-    }
-
-    pub mod fractals {
-        use rand;
-        use rand::prelude::*;
-        use rand::seq::SliceRandom;
-
-        pub fn chaos_game(
-            picture_size: (u32, u32),
-            iteration_depth: u32,
-        ) -> image::ImageBuffer<image::Rgb<u8>, std::vec::Vec<u8>> {
-            let mut image = image::RgbImage::new(picture_size.0, picture_size.1);
-
-            let steps = iteration_depth;
-
-            let a = (image.width() / 2, (image.height() as f32 * 0.02) as u32);
-            let b = (
-                (image.width() as f32 * 0.02) as u32,
-                (image.height() as f32 * 0.98) as u32,
-            );
-            let c = (
-                (image.width() as f32 * 0.98) as u32,
-                (image.height() as f32 * 0.98) as u32,
-            );
-
-            let point_array = vec![a, b, c];
-            let mut point = (image.width() / 2, image.width() / 2);
-
-            let mut rnd = thread_rng();
-
-            for _ in 0..steps {
-                let corner = point_array.as_slice().choose(&mut rnd).unwrap();
-                point = (
-                    (point.0 as i32 + ((corner.0 as i32 - point.0 as i32) / 2)) as u32,
-                    (point.1 as i32 + ((corner.1 as i32 - point.1 as i32) / 2)) as u32,
-                );
-
-                image.put_pixel(point.0, point.1, image::Rgb([255, 255, 255]));
-            }
-
-            image
         }
     }
 }
